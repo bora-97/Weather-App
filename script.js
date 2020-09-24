94dab96d11f6b023117a61d2b6a0c3b9981ae361
bora-97 <68867360+bora-97@users.noreply.github.com>
2020-09-24 17:40:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 console.log("Hello")
-$(document).ready(function){
-    $('#searchBtn').click(function){
-
+$(document).ready(function () {
+    $('#searchBtn').click(function () {
+       console.log("hello")
         var city = $('.form-control').val();
-        var APIkey = "8935db265b7cc0c65699dd8b71cd06b1";
-        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + APIkey;
+        var APIkey = "b5264e43577c6a0349a29e846da1212c";
+        var weatherQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + APIkey;
+        var forecastQueryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&APPID=" + APIkey;
+         $.ajax({
 
-        $.ajac({
-            
-          url: queryURL ,
+            url: weatherQueryURL,
 
-          method: "GET"
-        }).then(function(data)){
+            method: "GET"
+        }).then(function (data) {
 
-            if(city != " "){
-                
+            if (city != " ") {
+
                 var now = moment().format('l')
-                
+
                 console.log(data);
 
                 //append city name and date on dashboard
-             
-                var cityName = $(#'city-name');
+
+                var cityName = $('#city-name');
                 var weatherPic = data.weather[0].icon;
                 cityName.html(data.name + "" + now);
                 var currentPic = $("#current-pic");
-                currentPic.attr("src","https://openweathermap.org/img/wn/" + weatherPic +"@2x.png" )
+                currentPic.attr("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png")
 
-                var temperature = $('#temperature');
-                temperature.html("Temperature: " + data.main.temp + " " + "Farenheit");
+                var temperatureEl = $('#temperature');
+                temperatureEl.html("Temperature: " + data.main.temp + " " + "Farenheit");
 
                 var humidity = $("#humidity");
                 humidity.html("Humidity: " + data.main.humidity);
@@ -36,19 +36,26 @@ $(document).ready(function){
                 var windSpeed = $("#wind-speed");
                 windSpeed.html("Windspeed: " + data.wind.speed);
 
-                var UVindex = $("#UV-index");
                 
 
-            
+         
 
 
-            }else{
+                $.ajax({
+
+                    url: forecastQueryURL,
+        
+                    method: "GET"
+                }).then(function (forecastData) {
+                    console.log(forecastData)
+                })
+            } else {
                 $(".error").html("field empty");
             }
 
         })
 
-})
+    })
 
 
 
